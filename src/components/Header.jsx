962cfc0339085f420fc8c6de80f8400891ed7fe6
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-09 22:42:35 +0530

    Added color input

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Header = () => {
+  const temp = (e) => {
+    console.log(e.target.value);
+  }
+  return (
+    <header className="header">
+        <h1>Background Generator</h1>
+        <div className="subHead">
+            <div className="colorInput">
+                <input type="color" name="color1" id="color1" onChange={temp}/>
+                <span id="colorValue1"></span>
+                <input type="color" name="color2" id="color2"/>
+                <span id="colorValue2"></span>
+            </div>
+        </div>
+    </header>
+  )
+}
+
+export default Header
