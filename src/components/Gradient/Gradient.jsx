55b219d445018921f117349f70c78862fbbb5761
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-12 21:07:35 +0530

    Random

--- src/components/Gradient/Gradient.jsx
+++ src/components/Gradient/Gradient.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 
 const Gradient = () => {
-  const temp = (e) => {
+  const temp1 = (e) => {
+    console.log(e.target.value);
+  }
+  const temp2 = (e) => {
     console.log(e.target.value);
   }
   return (
     <div className="colorInput">
-      <input type="color" name="color1" id="color1" onChange={temp} />
+      <input type="color" name="color1" id="color1" onChange={temp1} />
       <span id="colorValue1"></span>
-      <input type="color" name="color2" id="color2" />
+      <input type="color" name="color2" id="color2" onChange={temp2}/>
       <span id="colorValue2"></span>
     </div>
 
--- src/components/Static/Static.jsx
+++ src/components/Static/Static.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Static.scss'
 
 const Static = () => {
   const temp = (e) => {
@@ -6,6 +7,7 @@ const Static = () => {
   }
   return (
     <div className="colorInput">
+      <label htmlFor="color1"> Select the color : </label>
       <input type="color" name="color1" id="color1" onChange={temp} />
       <span id="colorValue"></span>
     </div>
