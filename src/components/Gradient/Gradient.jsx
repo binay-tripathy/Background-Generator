be084cc65f3847d2fbc19f13563705340b75e64e
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-14 20:10:38 +0530

    Made some changes

--- src/components/Gradient/Gradient.jsx
+++ src/components/Gradient/Gradient.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
+import './Gradient.scss'
 
 const Gradient = () => {
   const color1 = (e) => {
-    console.log(e.target.value);
+    let color2 = document.querySelector('#color2');
+    console.log(color2.value);
+    document.getElementById('backgradient').style.backgroundImage = 'linear-gradient(to right, #525733, #57C300)';
   }
   const color2 = (e) => {
     console.log(e.target.value);
   }
   return (
-    <div className="colorInput">
-      <label htmlFor="color1"> Select the first color : </label>
-      <input type="color" name="color1" id="color1" onChange={color1} />
-      <span id="colorValue1"></span>
-      <label htmlFor="color2"> Select the second color : </label>
-      <input type="color" name="color2" id="color2" onChange={color2}/>
-      <span id="colorValue2"></span>
-    </div>
+    <>
+      <div className="colorInputg">
+        <div className="color1">
+          <label htmlFor="color1"> Select the first color : </label>
+          <input type="color" name="color1" id="color1" onChange={color1} />
+          <span id="colorValue1"></span>
+        </div>
+        <div className="color2">
+          <label htmlFor="color2"> Select the second color : </label>
+          <input type="color" name="color2" id="color2" onChange={color2} />
+          <span id="colorValue2"></span>
+        </div>
+      </div>
+      <div id='backgradient'>
+      </div>
+    </>
 
   )
 }
--- src/components/Static/Static.jsx
+++ src/components/Static/Static.jsx
@@ -3,14 +3,15 @@ import './Static.scss'
 
 const Static = () => {
   const color = (e) => {
+    console.log(e.target.value)
     document.getElementById('backsolid').style.background = e.target.value;
   }
   
   return (
     <>
-    <div className="colorInput">
+    <div className="colorInputs">
       <label htmlFor="color"> Select the color : </label>
-      <input type="color" name="color" id="color" onChange={color} />
+      <input type="color" name="color" id="color" defaultValue={'#ffffff'} onChange={color} />
       <span id="colorValue"></span>
     </div>
     <div id='backsolid'>
