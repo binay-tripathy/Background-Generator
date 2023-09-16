0a9ea689bc6c18500c9850208c59ff67cb4d3abc
Binay Tripathy <binaytripathy123@gmail.com>
2023-09-16 20:38:39 +0530

    Added the get code button

--- src/components/Gradient/Gradient.jsx
+++ src/components/Gradient/Gradient.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import './Gradient.scss'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCode } from '@fortawesome/free-solid-svg-icons'
 
 const Gradient = () => {
   const [g1, setG1] = useState('#ffffff');
   const [g2, setG2] = useState('#ffffff');
+  const [bgText, setBgText] = useState('linear-gradient(to right, #ffffff, #ffffff)');
 
   useEffect(() => {
     const bgcolor = `linear-gradient(to right, ${g1}, ${g2})`;
     document.getElementById('backgradient').style.background = bgcolor;
-    console.log(bgcolor);
   }, [g1, g2])
 
   const color1 = (e) => {
@@ -30,8 +32,13 @@ const Gradient = () => {
           <input type="color" name="color2" defaultValue={g2} id="color2" onChange={color2} />
           <span id="colorValue2"></span>
         </div>
+
       </div>
       <div id='backgradient'>
+        <button className="text">
+        <h6>Get the Code !!</h6>
+          <FontAwesomeIcon icon={faCode} beat size="xs" id='faicon' />
+        </button>
       </div>
     </>
 
